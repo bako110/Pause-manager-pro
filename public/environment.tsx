37933767bace725053d0da521e3d19de6ab8cce2
bako110 <bakorobert2000@gmail.com>
2025-11-04 16:26:32 +0100

    Changement de l'url backend :https://waste-6k43.onrender.com/

--- public/environment.tsx
+++ public/environment.tsx
@@ -1,3 +1,3 @@
 // src/config.js
 // export const API_BASE_URL = 'http://localhost:5000/api';
-export const API_BASE_URL = 'https://pause-manger-backend.onrender.com';
+export const API_BASE_URL = 'https://pause-manger-backend.onrender.com/api';
